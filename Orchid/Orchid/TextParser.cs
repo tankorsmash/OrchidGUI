2874efb50bc50fcff543e86c8087522c6226f223
TankorSmash <tankorsmash@gmail.com>
2012-08-26 17:54:56 -0400

    Actually add TextParser.cs

---
+++ Orchid/Orchid/TextParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orchid
+{
+    /// <summary>
+    /// Will be some form of text parsing. Bold, italics etc.
+    /// </summary>
+    class TextParser
+    {
+    }
+}
