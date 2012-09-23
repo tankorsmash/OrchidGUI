657865bc7ccf131aec95f4189b3ae3443997085f
TankorSmash <tankorsmash@gmail.com>
2012-09-23 16:11:53 -0400

    reworked dragging the left side of a surface
    There's still some stuff to work out, like having the surface dragged
    beyond 0, so the size becomes -1 and it crashes, but I think that's
    something I can fix just by adding a minimum size to the Surface class.
    something like 10x10 or something. Who knows.
    
    Anyways the issue right now is that it only drags the right side in and
    out, because resizing only affects the width of the surface, and since the
    left side is anchored by the bounding rect, the right side gets expanded
    everytime.
    
    I think the way to solve that is the add the diff between the new size and
    old side onto the X position of the bounding box and just change that.
    We'll see how it goes.

--- Orchid/Orchid/Input.cs
+++ Orchid/Orchid/Input.cs
@@ -50,6 +50,9 @@ namespace Orchid
         KeyboardState currentKeyState = Keyboard.GetState();
         KeyboardState lastKeyState = Keyboard.GetState();
 
+        //bool guiFound = false;
+
+
         public InputHandler(Game1 game)
         {
             //save a refernce to the main game, to easily access its attributes
@@ -70,8 +73,9 @@ namespace Orchid
 
             //this.theGame.testArea.Drag(currentMouseState, lastMouseState);
 
-            //if mouse1 is pressed
-            if (currentMouseState.LeftButton == ButtonState.Pressed)
+            //if mouse1 is pressed, but not dragging
+            if (currentMouseState.LeftButton == ButtonState.Pressed &
+                lastMouseState.LeftButton  != ButtonState.Pressed)
             {
                 //loop over the elements in the gui list 
                 foreach (GuiElement elem in guiElementList)
@@ -98,23 +102,39 @@ namespace Orchid
                             string text = String.Format("{0}, the Surface, has mouse down", castedElem);
                             theGame.msgList.Add(text);
 
-                            //if mousepos is along the left side of the box
-                                // make sure it's not too high
-                            if ((castedElem.rect.Top < mousePos.Y &
-                                mousePos.Y < castedElem.rect.Bottom) 
-                                &
-                                // make sure its not too far the the left or right.
-                                (castedElem.rect.Left-(castedElem.rect.Width *.1)  < mousePos.X) &
-                                (castedElem.rect.Left+(castedElem.rect.Width *.1) > mousePos.X))
-                            {
-                                string text2 = String.Format("{0}, the Surface, has mouse in", castedElem);
-                                theGame.msgList.Add(text2);
-                                Console.WriteLine("RESIZING");
-                                castedElem.Resize(currentMouseState, lastMouseState);
+                            //activate the pressed element
+                            activeElement = castedElem;
+                            
 
-                                
-                            }
                         }
+                    }
+                }
+            }
+
+
+            
+            //if the mouse1 button is being dragged
+            if (currentMouseState.LeftButton == ButtonState.Pressed &
+                lastMouseState.LeftButton == ButtonState.Pressed)
+            {                             //if mousepos is along the left side of the box
+                // make sure it's not too high
+
+                if (activeElement is Surface)
+                {
+                    Surface castedElem = (Surface)activeElement;
+                    if ((castedElem.rect.Top < mousePos.Y &
+                        mousePos.Y < castedElem.rect.Bottom) )
+                        //&
+                        //// make sure its not too far the the left or right.
+                        //(castedElem.rect.Left - (castedElem.rect.Width * .1) < mousePos.X) &
+                        //(castedElem.rect.Left + (castedElem.rect.Width * .1) > mousePos.X))
+                    {
+                        string text2 = String.Format("{0}, the Surface, has mouse in", castedElem);
+                        theGame.msgList.Add(text2);
+                        Console.WriteLine("RESIZING");
+                        castedElem.Resize(currentMouseState, lastMouseState);
+
+
                     }
                 }
             }
@@ -148,7 +168,7 @@ namespace Orchid
 
             }
 
-
+            //if mouse2 is pressed
             if  (lastMouseState.RightButton == ButtonState.Pressed)
             {
                 //Console.WriteLine("trying for a mouse up");
